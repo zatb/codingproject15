6f2b8d5f80986c63c3c9e0041a8772c02b8fd8fa
zatb <zatbolanos@gmail.com>
2024-07-05 21:20:51 -0400

    Add personal information section with styled components.

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+  <div className="App">
+    <header className="App-header">
+      <h1>Zach Bolanos</h1>
+      <p>
+        I'm currently a student at USF and am pursuing a degree in Business Analytics. I am set to gradutate in the Summer of 2025.
+      </p>
+      <p>Email: myemail@example.com | LinkedIn: linkedin.com/in/mylinkedin</p>
+    </header>
+  </div>
